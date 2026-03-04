f106d66364c8e667fca3fb27113eddb17da37b0e
KiNG PUBBA <101438644+king1pubba@users.noreply.github.com>
2026-03-04 15:03:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHABAN~k7lv",
+SESSION_ID: process.env.SESSION_ID || "𝕃𝔸ℝ𝔸-𝕄𝔻=LRp3FKwT#qZilQG7wfr3-OmQKC08wUClQXiaH7vQg6HMRI-eM4wk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 QUEEN SP BOT ALIVE NOW 💗*",
 BOT_OWNER: '94776290170',  // Replace with the owner's phone number
